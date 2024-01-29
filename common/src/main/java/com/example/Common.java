572ef4e5022b047ec796a47e8c3d8e7cbe5131f7
lex <lex@lex.lex>
2024-01-29 16:41:27 +0300

    initial junit

--- common/src/main/java/com/example/Common.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-public class Common {
-    public String commonMethod() {
-        System.out.println("log from common service");
-        return "value from common service";
-    }
-}
