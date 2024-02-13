6a52fe7455842380f72b7f6c4057a1d104abd856
lex <lex@lex.lex>
2024-02-13 15:57:12 +0300

    HibernateUtil

--- service/src/main/java/com/example/Service.java
+++ service/src/main/java/com/example/Service.java
@@ -1,34 +1,17 @@
 package com.example;
 
 import com.example.entity.Car;
-import com.example.entity.CarToMediaItem;
-import com.example.entity.Feedback;
-import com.example.entity.MediaItem;
-import com.example.entity.PersonalInfo;
-import com.example.entity.Request;
-import com.example.entity.User;
+import com.example.util.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.model.naming.CamelCaseToUnderscoresNamingStrategy;
-import org.hibernate.cfg.Configuration;
 
 public class Service {
 
     private final static Common common = new Common();
 
     public static void main(String[] args) {
-        Configuration configuration = new Configuration();
-        configuration.addAnnotatedClass(User.class);
-        configuration.addAnnotatedClass(PersonalInfo.class);
-        configuration.addAnnotatedClass(Car.class);
-        configuration.addAnnotatedClass(Request.class);
-        configuration.addAnnotatedClass(Feedback.class);
-        configuration.addAnnotatedClass(MediaItem.class);
-        configuration.addAnnotatedClass(CarToMediaItem.class);
-        configuration.setPhysicalNamingStrategy(new CamelCaseToUnderscoresNamingStrategy());
-        configuration.configure();
 
-        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
+        try (SessionFactory sessionFactory = HibernateUtil.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
             session.beginTransaction();
 
