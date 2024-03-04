b0cdd0b06f6636b69fcc559969e7cd13e138f18e
lex <lex@lex.lex>
2024-03-04 13:09:40 +0300

    personal info rep

---
+++ service/src/main/java/com/example/dao/PersonalInfoRepository.java
@@ -0,0 +1,10 @@
+package com.example.dao;
+
+import com.example.entity.PersonalInfo;
+import jakarta.persistence.EntityManager;
+
+public class PersonalInfoRepository extends BaseRepository<Long, PersonalInfo>{
+    public PersonalInfoRepository(EntityManager entityManager) {
+        super(PersonalInfo.class, entityManager);
+    }
+}
