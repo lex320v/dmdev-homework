03119f4ab2bfd9b820870e98b53a08be47b94aeb
lex <lex@lex.lex>
2024-03-04 13:09:19 +0300

    car test

---
+++ service/src/main/java/com/example/dao/MediaItemRepository.java
@@ -0,0 +1,10 @@
+package com.example.dao;
+
+import com.example.entity.MediaItem;
+import jakarta.persistence.EntityManager;
+
+public class MediaItemRepository extends BaseRepository<Long, MediaItem> {
+    public MediaItemRepository(EntityManager entityManager) {
+        super(MediaItem.class, entityManager);
+    }
+}
