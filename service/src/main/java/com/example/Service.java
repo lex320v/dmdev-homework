40674dcd7c9bfe564e2fe857e1e565405d6cd7a3
lex <lex@lex.lex>
2024-03-04 14:22:57 +0300

    refactoring

--- service/src/main/java/com/example/Service.java
+++ service/src/main/java/com/example/Service.java
@@ -1,29 +1,8 @@
 package com.example;
 
-import com.example.dao.MediaItemRepository;
-import com.example.dao.UserRepository;
-import com.example.dto.UserCreateDto;
-import com.example.entity.MediaItem;
-import com.example.entity.User;
-import com.example.entity.enums.Gender;
-import com.example.entity.enums.MediaItemType;
-import com.example.entity.enums.Role;
-import com.example.entity.enums.UserStatus;
-import com.example.interceptor.TransactionInterceptor;
-import com.example.mapper.PersonalInfoReadMapper;
-import com.example.mapper.UserCreateMapper;
-import com.example.mapper.UserReadMapper;
-import com.example.service.UserService;
 import com.example.util.HibernateUtil;
-import net.bytebuddy.ByteBuddy;
-import net.bytebuddy.implementation.MethodDelegation;
-import net.bytebuddy.matcher.ElementMatchers;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Proxy;
-import java.util.Optional;
 
 public class Service {
 
