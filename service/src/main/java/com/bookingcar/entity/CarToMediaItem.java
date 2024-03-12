51783c5dace5a2024cadf45aaaf638cad0fe3d46
lex <lex@lex.lex>
2024-03-12 22:15:06 +0300

    spring-boot-starter-data-jpa

--- service/src/main/java/com/bookingcar/entity/CarToMediaItem.java
+++ service/src/main/java/com/bookingcar/entity/CarToMediaItem.java
@@ -4,8 +4,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MapsId;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -22,11 +22,13 @@ public class CarToMediaItem implements IdentifiableEntity<CarToMediaItemId> {
     private CarToMediaItemId id;
 
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
-    @MapsId("car_id")
+    @JoinColumn(name = "car_id", referencedColumnName = "id", insertable = false, updatable = false)
+//    @MapsId("car_id")
     private Car car;
 
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
-    @MapsId("media_item_id")
+    @JoinColumn(name = "media_item_id", referencedColumnName = "id", insertable = false, updatable = false)
+//    @MapsId("media_item_id")
     private MediaItem mediaItem;
 
     @Column(nullable = false)
