0c9a5e5628bc8f33bf7203994da918afdb43c440
lex <lex@lex.lex>
2024-03-12 23:06:48 +0300

    refactoring

--- service/src/main/java/com/bookingcar/Service.java
+++ service/src/main/java/com/bookingcar/Service.java
@@ -1,12 +1,13 @@
 package com.bookingcar;
 
-import com.example.Common;
 import com.bookingcar.repository.UserRepository;
-import jakarta.persistence.EntityManager;
+import com.example.Common;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class Service {
 
     private final static Common common = new Common();
@@ -15,13 +16,9 @@ public class Service {
 
         var context = SpringApplication.run(Service.class, args);
 
-        var entityManager = context.getBean(EntityManager.class);
         var userRepository = context.getBean(UserRepository.class);
-        entityManager.getTransaction().begin();
         var t = userRepository.findAll();
 
         System.out.println(t);
-
-        entityManager.getTransaction().commit();
     }
 }
