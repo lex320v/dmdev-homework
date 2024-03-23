3c81825c1b5012f0efb638dbc702374f6627e586
lex <lex@lex.lex>
2024-03-23 11:34:00 +0300

    createdAt updatedAt hibernate annotations

--- service/src/main/java/com/bookingcar/config/AuditConfiguration.java
+++
@@ -1,9 +0,0 @@
-package com.bookingcar.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@EnableJpaAuditing
-@Configuration
-public class AuditConfiguration {
-}
--- service/src/main/java/com/bookingcar/entity/BaseEntity.java
+++ service/src/main/java/com/bookingcar/entity/BaseEntity.java
@@ -1,14 +1,12 @@
 package com.bookingcar.entity;
 
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.io.Serializable;
 import java.time.Instant;
@@ -18,12 +16,11 @@ import java.time.Instant;
 @Setter
 @Getter
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity<T extends Serializable> implements IdentifiableEntity<T> {
 
-    @CreatedDate
+    @CreationTimestamp
     private Instant createdAt;
 
-    @LastModifiedDate
+    @UpdateTimestamp
     private Instant updatedAt;
 }
