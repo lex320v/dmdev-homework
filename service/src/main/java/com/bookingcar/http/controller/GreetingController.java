07203ca0d0492c5bd9a0af1f571e0ab7d88c5ac1
lex <lex@lex.lex>
2024-03-26 15:55:01 +0300

    thymeleaf

--- service/src/main/java/com/bookingcar/http/controller/GreetingController.java
+++ service/src/main/java/com/bookingcar/http/controller/GreetingController.java
@@ -2,16 +2,15 @@ package com.bookingcar.http.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class GreetingController {
 
     @GetMapping("/hello")
-    public ModelAndView hello(ModelAndView modelAndView, @RequestParam Integer age) {
+    public ModelAndView hello(ModelAndView modelAndView) {
         System.out.println("###");
-        System.out.println(age);
+
         System.out.println("###");
 
         modelAndView.setViewName("greeting/hello");
